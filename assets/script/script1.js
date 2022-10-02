2513079bad959594411b1128665e0f93f3ca8c78
MargaritaDyakova <dyakova@tpcorp.ru>
2022-10-02 23:53:59 +0300

    second page rdy

--- assets/script/script1.js
+++ assets/script/script1.js
@@ -10,19 +10,19 @@ document.addEventListener("DOMContentLoaded", () => {
             body.classList.add('no-scroll-md')
         }
     })
-
+    
     let dropdownMenuOutside = document.querySelector('.outside-js')
     dropdownMenuOutside.addEventListener('click', () => {
         dropdownMenu.classList.remove('open')
         body.classList.remove('no-scroll')
         body.classList.remove('no-scroll-md')
     })
-
+    
     let burger = document.querySelector('.open-menu-js')
     burger.addEventListener('click', () => {
         burgerBtnHandler()
     })
-
+    
     function burgerBtnHandler() {
         if(!burger.classList.contains('opened')) {
             burger.classList.add('opened')
@@ -35,48 +35,48 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         headerMenu.classList.toggle('open')
     }
-
+    
     const body = document.querySelector('body')
-
+    
     const headerMenu = document.querySelector('.header__list-wrapper')
-
+    
     const swiper = new Swiper('.swiper', {
-        loop: true,
+        // loop: true,
         autoplay: {
             delay: 5000,
             disableOnInteraction: false
         },
-        width: 1200,
-        slidesPerView: 5,
         scrollbar: {
-            el: ".swiper-scrollbar",
-            // hide: true
+            el: ".swiper-scrollbar"
         },
+        slidesPerView: 4,
+        spaceBetween: 20,
+        width: 1500,
         breakpoints: {
             320: {
                 slidesPerView: 1.7,
                 spaceBetween: 16,
-                width: 300,
+                width: 280,
             },
             575.98: {
                 slidesPerView: 1.8,
                 spaceBetween: 16,
-                width: 400,
+                width: 500,
             },
             767.97: {
                 slidesPerView: 2.7,
                 spaceBetween: 20,
-                width: 800,
+                width: 700,
             },
             991.98: {
                 slidesPerView: 4,
                 spaceBetween: 20,
-                width: 1200,
+                width: 1000,
             },
             1199.98: {
-                slidesPerView: 5,
+                slidesPerView: 4,
                 spaceBetween: 20,
-                width: 1500,
+                width: 1200,
             }
         }
     })
@@ -95,17 +95,17 @@ document.addEventListener("DOMContentLoaded", () => {
             },
         }
     })
-
+    
     const formSection = document.querySelector('#form')
-
+    
     const orderBtn = document.querySelector('.order-btn-js')
-
+    
     orderBtn.addEventListener('click', () => {
         window.scrollTo({
             top: formSection.offsetTop + 30,
             behavior: "smooth"
         })
     })
-
+    
     document.querySelector('.sublist').addEventListener('click',(e) => e.stopPropagation())
 })
